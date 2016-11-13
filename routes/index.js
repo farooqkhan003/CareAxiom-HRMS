8f428b971e080d386f1fd8d5682665142a3f1b6e
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-11-13 14:36:07 +0500

    Testing README.md file

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/abc', function(req, res, next) {
+  res.send('Hey Nigga');
+});
+
 module.exports = router;
