68cb63c83fad4ec58cc8034f3ea6b58ab8bf0e13
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-11-29 22:54:43 +0500

    Minor additions

--- routes/employee.js
+++ routes/employee.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var user = require('../app/controllers/employee/user');
+var leave = require('../app/controllers/employee/leave');
+
 router.get('/', function(req, res, next) {
   res.render('profile');
 });
+
+router.post('/new', user.addNewUserQuick);
 
 router.get('/abc/:abc', function(req, res, next) {
   res.json({ abc : 'abc' });
