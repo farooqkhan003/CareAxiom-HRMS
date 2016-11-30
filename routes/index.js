b953df925e5d4641d203319442e898469e47a46b
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-11-30 18:38:41 +0500

    Adding controllers related to Module

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
-  global.db.User.addUserComplete('abcde', 'abc@abc.com', 'mmmmm', 'admin', 'ab', 'cba', 'Software Developer',
-    null, null, '10', 'PKR', 'pending', new Date().getMonth(), new Date().getYear(), '0', '0');
-  global.db.User.addUserComplete('abcdef', 'ab@abc.com', 'mmmmm', 'engineer', 'ab', 'cba', 'Software Developer',
-    null, null, '10', 'PKR', 'pending', new Date().getMonth(), new Date().getYear(), '0', '0');
+  if(req.isAuthenticated()) {
+    var redirectURL = '/profile?user=' + req.user.userName;
+    return res.redirect(redirectURL);
+  }
+  else {
+    global.db.User.addUserComplete('abcde', 'abc@abc.com', 'mmmmm', 'admin', 'ab', 'cba', 'Software Developer',
+      null, null, '10', 'PKR', 'pending', new Date().getMonth(), new Date().getYear(), '0', '0');
+    global.db.User.addUserComplete('abcdef', 'ab@abc.com', 'mmmmm', 'engineer', 'ab', 'cba', 'Software Developer',
+      null, null, '10', 'PKR', 'pending', new Date().getMonth(), new Date().getYear(), '0', '0');
 
-  res.render('login', { message : req.flash('message') });
-
+    res.render('login', { message : req.flash('message') });
+  }
 });
 
 module.exports = router;
