ced88e93116a712e74a1853235c6a9e13cce46c1
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-12-01 02:24:34 +0500

    Directory view almost complete

--- app/controllers/directory.js
+++ app/controllers/directory.js
@@ -1,4 +1,32 @@
+var _ = require('underscore');
 
 exports.viewDirectory = function (req, res, next) {
-  return res.render('directory');
+  var _users;
+  return global.db.User.getAllUsers()
+    .then(function (users) {
+      _users = users;
+      var userIds = [1, 2];
+      return global.db.UserInfo.getAllUsersInfoByUserIds(userIds);
+    }).then(function (usersInfo) {
+
+      var employeesObj = _.map(usersInfo, function (userInfo) {
+        return {
+          name: userInfo.getName(),
+          designation: userInfo.get('designation'),
+          email: null, // Retrieve user email from _users based on user_id
+          phone: userInfo.get('contact_no'),
+          address: userInfo.get('address')
+        };
+      });
+
+      return res.render('directory', { employees : employeesObj });
+    }).catch(function (err) {
+
+    });
+
+  // employee.name %></td>
+  // <td><%= employee.designation %></td>
+  // <td><%= employee.email %></td>
+  // <td><%= employee.phone %></td>
+  // <td><%= employee.address %></td>
 };
