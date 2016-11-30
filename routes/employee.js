8f1735d653e80887c39aee07a6dcca8fd0234d02
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-11-30 19:20:37 +0500

    No changes

--- routes/employee.js
+++ routes/employee.js
@@ -5,17 +5,9 @@ var user = require('../app/controllers/user');
 var leave = require('../app/controllers/leave');
 var directory = require('../app/controllers/directory');
 
-router.get('/', function(req, res, next) {
-  res.render('profile');
-});
 
 router.post('/new', user.addNewUserQuick);
 router.post('/leave/apply', leave.applyForLeave);
 router.get('/directory/view', directory.viewDirectory);
-
-router.get('/abc/:abc', function(req, res, next) {
-  res.json({ abc : 'abc' });
-  // req.params.abc
-});
 
 module.exports = router;
