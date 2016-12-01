945040bbe78bc66e9fb61936ad9fe28731fd0abe
Farooq Khan <farooqahmadkhan003@gmail.com>
2016-12-01 18:12:26 +0500

    Add user with ajax added.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var calendar = require('./routes/calendar');
 var employee = require('./routes/employee');
 var company = require('./routes/company');
 var authentication = require('./app/middlewares/authentication');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -36,19 +37,20 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
-  secret: 'SpeaktaVeryGoodEngrish',
-  saveUninitialized: true,
-  resave: false
+    secret: 'SpeaktaVeryGoodEngrish',
+    saveUninitialized: true,
+    resave: false
 }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
 });
+
 
 app.use('/', index);
 app.use('/profile', authentication.isAuthenticated, profile);
@@ -56,42 +58,42 @@ app.use('/calendar', authentication.isAuthenticated, calendar);
 app.use('/employee', authentication.isAuthenticated, employee);
 app.use('/company', company);
 app.post('/login', function (req, res, next) {
-  passport.authenticate('local.signIn', function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      req.flash('message', info.message);
-      return res.redirect('/');
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      var redirectURL = '/profile/view?user=' + user.get('username');
-      return res.redirect(redirectURL);
-    });
-  })(req, res, next);
+    passport.authenticate('local.signIn', function (err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            req.flash('message', info.message);
+            return res.redirect('/');
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            var redirectURL = '/profile/view?user=' + user.get('username');
+            return res.redirect(redirectURL);
+        });
+    })(req, res, next);
 });
 app.get('/logout', authentication.destroySession);
 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
