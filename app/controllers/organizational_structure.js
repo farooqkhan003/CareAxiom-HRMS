cdbb42eb234459c45fe8800637e048aa78f44875
Khawaja Muhammad Ahsen <ahsenkh@gmail.com>
2016-12-01 15:08:32 +0500

    Removed merge conflict between awais and master

--- app/controllers/organizational_structure.js
+++ app/controllers/organizational_structure.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 /*
  * @author Khawaja Ahsen
  * created on 29/11/2016
  */
-=======
 var _ = require('underscore');
 
 exports.viewOrganizationalStructure = function (req, res, next) {
     return res.render('organizational_structure', {});
 };
->>>>>>> master
